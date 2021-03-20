32cfba4641bc4cb2ad79be6c3cf14d6b0c20a902
deryakilic <ayredkklc@unc.edu>
2021-03-20 11:50:34 -0400

    hi2

--- main.py
+++ main.py
@@ -3,11 +3,13 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import numpy as np
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+    x = np.abs(-3)
+    print(x)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
