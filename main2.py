a3e99ee63a75fd47fa507b535916419441d571d2
dipen321 <dipen_baps@yahoo.ca>
2021-03-20 18:45:04 -0400

    -Add number Of Ingredients

--- main2.py
+++ main2.py
@@ -6,6 +6,8 @@ hits_dictionary = data['hits']'''
 foodname = input('What food are you craving? ')
 
 yourIngredients = [item for item in input("Enter the ingredients that you want to use : ").split()]
+numberOfIngredients = input('Enter the total number of ingredients you want to use: ')
+
 
 print('Recipes that match your ingredient ')
 print()
