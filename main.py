487428a1b636ad8c93032df5f7a9a43087e10a92
djkang17 <djkai0839@gmail.com>
2021-03-20 12:24:47 -0400

    added name

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Dipen')
+    print_hi('Kai')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
